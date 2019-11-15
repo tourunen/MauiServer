f1cf1ecdc9abd83d585b2f2c868ba7ccc834b760
Juho Inkinen <juho.inkinen@helsinki.fi>
2019-11-15 12:26:37 +0200

    Switch username to tomcat_user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,9 @@ MY_UID=${MY_UID:-9001}
 
 echo "Starting with UID: $MY_UID"
 groupadd -r $MY_UID -g $MY_GID
-useradd --shell /bin/bash -u $MY_UID -g $MY_GID -o -c "" -m annif_user
+useradd --shell /bin/bash -u $MY_UID -g $MY_GID -o -c "" -m tomcat_user
 
 chown -R $MY_UID:$MY_GID /usr/local/tomcat
 chown -R $MY_UID:$MY_GID /mauidata
 
-exec gosu annif_user "$@"
+exec gosu tomcat_user "$@"
